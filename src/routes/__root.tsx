40b86069a8165509612e96897d8216e2f19a3484
Deepak Murugan <deepakmurugan@gmail.com>
2026-08-04 16:36:06 +0530

    Describe your changes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,6 +9,7 @@ import {
 import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
+import faviconUrl from "../assets/favi.png";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { FloatingWhatsApp } from "../components/site/FloatingWhatsApp";
 import { PageTransition } from "../components/site/PageTransition";
@@ -84,6 +85,9 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
+      { rel: "icon", href: faviconUrl, type: "image/png" },
+      { rel: "shortcut icon", href: faviconUrl, type: "image/png" },
+      { rel: "apple-touch-icon", href: faviconUrl, type: "image/png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
